b0f7033b5381b200f572dc98cc5b15c22f1d4c45
YoannDupont <yoa.dupont@gmail.com>
2020-12-11 20:23:40 +0100

    Day 10 and 11.

---
+++ src/day10.adb
@@ -0,0 +1,84 @@
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+with Ada.Command_Line;
+with Ada.Containers.Generic_Array_Sort;
+
+procedure Day10 is
+    package TIO renames Ada.Text_IO;
+    package ITIO renames Ada.Integer_Text_IO;
+
+    type Joltage_Array is array(Positive range <>) of Natural;
+    Empty_Joltage_Array : constant Joltage_Array(1 .. 0) := (others => <>);
+
+    subtype Long_Long_Natural is Long_Long_Integer range 0 .. Long_Long_Integer'Last;
+
+    procedure Sort is new Ada.Containers.Generic_Array_Sort(Positive, Natural, Joltage_Array);
+
+    function Get(F : in TIO.File_Type) return Joltage_Array is
+        function Get_Rec(acc : Joltage_Array) return Joltage_Array is
+            P : Positive;
+        begin
+            if TIO.End_Of_File(F) then
+                return acc;
+            end if;
+            ITIO.Get(F, P);
+            return Get_Rec(acc & P);
+        end Get_Rec;
+    begin
+        return Get_Rec(Empty_Joltage_Array);
+    end Get;
+
+    procedure Part_1(adapters : in Joltage_Array; ones, threes : out Natural) is
+        data : Joltage_Array := adapters;
+        previous : Natural;
+    begin
+        ones := 0;
+        threes := 1;
+        Sort(data);
+        previous := 0;
+        for joltage of data loop
+            if joltage - previous = 1 then
+                ones := ones + 1;
+            elsif joltage - previous = 3 then
+                threes := threes + 1;
+            end if;
+            previous := joltage;
+        end loop;
+    end Part_1;
+
+    procedure Part_2(adapters : in Joltage_Array; number_of_arrangements : out Long_Long_Natural) is
+        data : Joltage_Array := adapters & (1 => 0);
+        number_of_alternatives : array(data'Range) of Long_Long_Natural := (others => 0);
+    begin
+        Sort(data);
+        number_of_alternatives(number_of_alternatives'First) := 1;
+        for I in data'First + 1 .. data'Last loop
+            for J in reverse Positive'Max(I - 3, data'First) .. I - 1 loop
+                if data(I) - data(J) <= 3 then
+                    number_of_alternatives(I) := number_of_alternatives(I) + number_of_alternatives(J);
+                end if;
+            end loop;
+        end loop;
+        number_of_arrangements := number_of_alternatives(number_of_alternatives'Last);
+    end Part_2;
+
+    filepath : constant String := Ada.Command_Line.Argument(1);
+    F : TIO.File_Type;
+begin
+    TIO.Open(F, TIO.In_File, filepath);
+    declare
+        adapters : constant Joltage_Array := Get(F);
+        ones, threes : Natural;
+        number_of_arrangements : Long_Long_Natural;
+    begin
+        Part_1(adapters, ones, threes);
+        TIO.Put_Line("What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?");
+        ITIO.Put(ones * threes, width => 0);
+        TIO.New_Line;
+        TIO.New_Line;
+        Part_2(adapters, number_of_arrangements);
+        TIO.Put_Line("What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?");
+        TIO.Put(number_of_arrangements'Img);
+    end;
+    TIO.Close(F);
+end Day10;
---
+++ src/day11.adb
@@ -0,0 +1,170 @@
+with Ada.Text_IO;
+with Ada.Command_Line;
+
+procedure Day11 is
+    package TIO renames Ada.Text_IO;
+
+    type State is ('.', 'L', '#');
+    subtype Seat_State is State range 'L' .. '#';
+    type Grid is array(Positive range <>, Positive range <>) of State;
+    type Integer_Couple is array(1 .. 2) of Integer;
+
+    function To_State(C : in Character) return State is
+    begin
+        return State'Value(''' & C & ''');
+    end To_State;
+
+    function To_Value(S : in State) return Natural is
+    begin
+        case S is
+            when '#' => return 1;
+            when '.' | 'L' => return 0;
+        end case;
+    end To_Value;
+
+    function Line_Count(F : in out TIO.File_Type) return Natural is
+        count : Natural := 0;
+    begin
+        TIO.Reset(F, TIO.In_File);
+        while not TIO.End_Of_File(F) loop
+            count := count + 1;
+            TIO.Skip_Line(F);
+        end loop;
+        TIO.Reset(F, TIO.In_File);
+        return count;
+    end Line_Count;
+
+    function Line_Length(F : in out TIO.File_Type) return Natural is
+        length : Natural;
+    begin
+        TIO.Reset(F, TIO.In_File);
+        length := TIO.Get_Line(F)'Length;
+        TIO.Reset(F, TIO.In_File);
+        return length;
+    end Line_Length;
+
+    function Get(F : in out TIO.File_Type) return Grid is
+        LC : constant Positive := Line_Count(F);
+        LL : constant Positive := Line_Length(F);
+        buffer : String(1 .. LL);
+        G : Grid(1 .. LC, 1 .. LL);
+    begin
+        for I in G'Range(1) loop
+            TIO.Get(F, buffer);
+            for J in buffer'Range loop
+                G(I, J) := To_State(buffer(J));
+            end loop;
+            if TIO.End_Of_File(F) then
+                TIO.Skip_Line(F);
+            end if;
+        end loop;
+        return G;
+    end Get;
+
+    function Part_1(G: in Grid; I, J : in Positive) return Natural is
+        count : Natural := 0;
+    begin
+        for x in Positive'Max(G'First(1), I -1) .. Positive'Min(G'Last(1), I + 1) loop
+            for y in Positive'Max(G'First(2), J - 1) .. Positive'Min(G'Last(2), J + 1) loop
+                if x /= I or y /= J then
+                    count := count + To_Value(G(x, y));
+                end if;
+            end loop;
+        end loop;
+        return count;
+    end Part_1;
+
+    function Part_2(G: in Grid; I, J : in Positive) return Natural is
+        shifts : constant array(1 .. 8) of Integer_Couple := (
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        );
+        count : Natural := 0;
+        x, y : Natural;
+    begin
+        for shift of shifts loop
+            x := I + shift(1);
+            y := J + shift(2);
+            while x in G'Range(1) and y in G'Range(2) loop
+                if G(x, y) in Seat_State then
+                    count := count + To_Value(G(x, y));
+                    exit;
+                end if;
+                x := x + shift(1);
+                y := y + shift(2);
+            end loop;
+        end loop;
+        return count;
+    end Part_2;
+
+    function Next(
+        G : in Grid;
+        Count_Neighbours : access function(G : in Grid; x,y : Positive) return Natural;
+        tolerance : in Natural
+    ) return Grid is
+        nxt : Grid(G'Range(1), G'Range(2)) := G;
+        count : Natural;
+    begin
+        for I in G'Range(1) loop
+            for J in G'Range(2) loop
+                case G(I, J) is
+                    when '.' => null;
+                    when 'L' =>
+                        count := Count_Neighbours(G, I, J);
+                        if count = 0 then
+                            nxt(I, J) := '#';
+                        end if;
+                    when '#' =>
+                        count := Count_Neighbours(G, I, J);
+                        if count >= tolerance then
+                            nxt(I, J) := 'L';
+                        end if;
+                end case;
+            end loop;
+        end loop;
+        return nxt;
+    end Next;
+
+    function Occupied_Seats(G : Grid) return Natural is
+        count : Natural := 0;
+    begin
+        for seat of G loop
+            count := count + To_Value(seat);
+        end loop;
+        return count;
+    end Occupied_Seats;
+
+    function Equilibirum_Occupation(
+        G : in Grid;
+        Count_Neighbours : access function(G : in Grid; x,y : Positive) return Natural;
+        tolerance : in Natural
+    ) return Natural is
+        current : Grid := G;
+        nxt : Grid(G'Range(1), G'Range(2));
+    begin
+        loop
+            nxt := Next(current, Count_Neighbours, tolerance);
+            exit when current = nxt;
+            current := nxt;
+        end loop;
+        return Occupied_Seats(current);
+    end Equilibirum_Occupation;
+
+    filepath : constant String := Ada.Command_Line.Argument(1);
+    F : TIO.File_Type;
+begin
+    TIO.Open(F, TIO.In_File, filepath);
+    declare
+        G : constant Grid := Get(F);
+        p1 : constant Natural := Equilibirum_Occupation(G, Part_1'Access, 4);
+        p2 : constant Natural := Equilibirum_Occupation(G, Part_2'Access, 5);
+    begin
+        TIO.Put_Line("How many seats end up occupied?");
+        TIO.Put_Line(p1'Img);
+        TIO.New_Line;
+        TIO.Put_Line("Given the new visibility method and the rule change for occupied seats becoming empty, once equilibrium is reached, how many seats end up occupied?");
+        TIO.Put_Line(p2'Img);
+    end;
+    TIO.Close(F);
+end Day11;
