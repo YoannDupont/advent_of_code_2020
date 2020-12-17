0d80ceeea33956d6a916cebaa9c64992e0650551
YoannDupont <yoa.dupont@gmail.com>
2020-12-17 19:04:08 +0100

    Day 17: added a combination with replacement function to look for neighbours.

--- src/day17.adb
+++ src/day17.adb
@@ -6,7 +6,10 @@ with Ada.Command_Line;
 procedure Day17 is
     package TIO renames Ada.Text_IO;
 
-    type Coordinate is array(1 .. 4) of Integer;
+    subtype Dimension is Positive range 1 .. 4;
+    type Coordinate is array(Dimension'Range) of Integer;
+    Origin : constant Coordinate := (others => 0);
+    type Coordinate_Array is array(Positive range <>) of Coordinate;
 
     package Coordinate_Sets is new Ada.Containers.Ordered_Sets(Coordinate);
     subtype Grid4D is Coordinate_Sets.Set;
@@ -14,24 +17,57 @@ procedure Day17 is
     package Coordinate_To_Integer is new Ada.Containers.Ordered_Maps(Coordinate, Integer);
     subtype C2I is Coordinate_To_Integer.Map;
 
+    function "+"(L, R : in Coordinate) return Coordinate is
+        C : Coordinate := Origin;
+    begin
+        for I in Dimension loop
+            C(I) := L(I) + R(I);
+        end loop;
+        return C;
+    end "+";
+
+    function Combinations_With_Replacement
+       (size : Dimension;
+        lo, hi: in Integer;
+        keep_origin : Boolean := True) return Coordinate_Array
+    is
+        buffer : Coordinate_Array(1 .. (hi - lo + 1)**size);
+        current : Coordinate := (others => 0);
+        index : Positive := buffer'First;
+
+        procedure Generate(nth : Dimension) is
+        begin
+            for I in lo .. hi loop
+                current(nth) := I;
+                if nth = size then
+                    if current /= Origin or else keep_origin then
+                        buffer(index) := current;
+                        index := index + 1;
+                    end if;
+                else
+                    Generate(nth + 1);
+                end if;
+            end loop;
+        end Generate;
+    begin
+        Generate(Dimension'First);
+        return buffer(buffer'first .. index - 1);
+    end Combinations_With_Replacement;
+
     function Get(F : in TIO.File_Type) return Grid4D is
         grid : Grid4D;
         x, y : Integer;
     begin
         y := 0;
         while not TIO.End_Of_File(F) loop
-            declare
-                line : constant String := TIO.Get_Line(F);
-            begin
-                x := 0;
-                for C of line loop
-                    if C = '#' then
-                        grid.Insert((x, y, 0, 0));
-                    end if;
-                    x := x + 1;
-                end loop;
-                y := y + 1;
-            end;
+            x := 0;
+            for C of TIO.Get_Line(F) loop
+                if C = '#' then
+                    grid.Insert((x, y, 0, 0));
+                end if;
+                x := x + 1;
+            end loop;
+            y := y + 1;
         end loop;
         return grid;
     end Get;
@@ -41,45 +77,26 @@ procedure Day17 is
         Element := Element + 1;
     end Increment;
 
-    function Count_Immediate_Neighbours(grid : in Grid4D; use_w : Boolean) return C2I is
+    function Count_Immediate_Neighbours(grid : in Grid4D; n_dims : Dimension) return C2I is
         counts : C2I;
         neighbour : Coordinate;
     begin
         for coord of grid loop
-            for x in -1 .. 1 loop
-                for y in -1 .. 1 loop
-                    for z in -1 .. 1 loop
-                        if not use_w then
-                            if not(x = 0 and y = 0 and z = 0) then
-                                neighbour := (coord(1)+x, coord(2)+y, coord(3)+z, 0);
-                                if counts.Contains(neighbour) then
-                                    counts.Update_Element(counts.Find(neighbour), Increment'Access);
-                                else
-                                    counts.Insert(neighbour, 1);
-                                end if;
-                            end if;
-                        else
-                            for w in -1 .. 1 loop
-                                if not(x = 0 and y = 0 and z = 0 and w = 0) then
-                                    neighbour := (coord(1)+x, coord(2)+y, coord(3)+z, coord(4)+w);
-                                    if counts.Contains(neighbour) then
-                                        counts.Update_Element(counts.Find(neighbour), Increment'Access);
-                                    else
-                                        counts.Insert(neighbour, 1);
-                                    end if;
-                                end if;
-                            end loop;
-                        end if;
-                    end loop;
-                end loop;
+            for shift of Combinations_With_Replacement(n_dims, -1, 1, False) loop
+                neighbour := coord + shift;
+                if counts.Contains(neighbour) then
+                    counts.Update_Element(counts.Find(neighbour), Increment'Access);
+                else
+                    counts.Insert(neighbour, 1);
+                end if;
             end loop;
         end loop;
         return counts;
     end Count_Immediate_Neighbours;
 
-    function Next(grid : in Grid4D; use_w : Boolean) return Grid4D is
+    function Next(grid : in Grid4D; n_dims : Dimension) return Grid4D is
         nxt : Grid4D;
-        counts : constant C2I := Count_Immediate_Neighbours(grid, use_w);
+        counts : constant C2I := Count_Immediate_Neighbours(grid, n_dims);
         C : Coordinate_To_Integer.Cursor := counts.First;
         cell : Coordinate;
     begin
@@ -99,27 +116,27 @@ procedure Day17 is
         return nxt;
     end Next;
 
+    function Cycle(G : in Grid4D; n_dims : Dimension) return Grid4D is
+        final : Grid4D := G.Copy;
+    begin
+        for I in 1 .. 6 loop
+            final := Next(final, n_dims);
+        end loop;
+        return Final;
+    end Cycle;
+
     filepath : constant String := Ada.Command_Line.Argument(1);
     F : TIO.File_Type;
     grid : Grid4D;
-    part_1 : Grid4D;
-    part_2 : Grid4D;
 begin
     TIO.Open(F, TIO.In_File, filepath);
     grid := Get(F);
     TIO.Close(F);
 
-    part_1 := grid.Copy;
-    for I in 1 .. 6 loop
-        part_1 := Next(part_1, False);
-    end loop;
     TIO.Put_Line("Starting with your given initial configuration, simulate six cycles. How many cubes are left in the active state after the sixth cycle?");
-    TIO.Put_Line(part_1.Length'Img);
+    TIO.Put_Line(Cycle(grid, 3).Length'Img);
+    TIO.New_Line;
 
-    part_2 := grid.Copy;
-    for I in 1 .. 6 loop
-        part_2 := Next(part_2, True);
-    end loop;
     TIO.Put_Line("Starting with your given initial configuration, simulate six cycles in a 4-dimensional space. How many cubes are left in the active state after the sixth cycle?");
-    TIO.Put_Line(part_2.Length'Img);
+    TIO.Put_Line(Cycle(grid, 4).Length'Img);
 end Day17;
